c282d19a665857cd01a0313952d5aebab7ace2f7
carson <carsin@users.noreply.github.com>
2017-09-12 12:27:19 -1000

    nav2js: initial version

--- dist/components/nav/2.0/nav2.js
+++ dist/components/nav/2.0/nav2.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-    $(".fa-bars").click(function() {
-        $(".text-menu").toggleClass('menu-appear');
+    $(".mobile-toggle").click(function() {
+        $(".nav-items").toggleClass("menu-appear");
     });
   
-    $("nav .text-menu a").click(function() {
-        $(".text-menu").toggleClass('menu-appear');
+    $(".nav .nav-items a").click(function() {
+        $(".nav-items").toggleClass("menu-appear");
     });
 });
