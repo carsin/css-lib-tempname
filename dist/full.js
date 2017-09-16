046f6f95a2dff24af92d007c8050a6906acdd9ed
carsin <carsin@users.noreply.github.com>
2017-09-16 10:27:47 -1000

    improve mobile nav

--- dist/full.js
+++ dist/full.js
@@ -5,18 +5,42 @@
 
 /* -----------[ 1. Navbar ]---------- */
 
+var isFixed;
+
 $(document).ready(function() {
+    isFixed = $(".nav").hasClass("fixed");
     $(".mobile-toggle").click(function() {
-        $(".mobile-toggle").toggleClass("toggled");
-        $("body").toggleClass("no-scroll");
-        $(".nav-items").toggleClass("nav-appear");
-        $(".nav").toggleClass("toggled");
-    });
-  
-    $(".nav.toggled .nav-items a").click(function() {
-        $(".mobile-toggle").toggleClass("toggled");
-        $("body").toggleClass("no-scroll");
-        $(".nav-items").toggleClass("nav-appear");
-        $(".nav").toggleClass("toggled");
+        toggleNavElements();
+        if ($(".mobile-toggle").hasClass("mobile-toggled")) {
+            addNavDropdownLinks();
+        } else {
+            setTimeout(removeNavDropdownLinks, 500);
+        }
+
+        $(".nav-appear a").click(function() {
+            toggleNavElements();
+            removeNavDropdownLinks();
+        });
     });
 });
+
+function toggleNavElements() {
+    $(".mobile-toggle").toggleClass("mobile-toggled");
+    $(".nav-items").toggleClass("nav-appear");
+    $(".nav").toggleClass("nav-toggled");
+    if (!isFixed) { $(".nav").toggleClass("fixed"); }
+}
+
+function addNavDropdownLinks() {
+    console.log("add");
+    $(".nav-dropdown-item ul li").each(function() {
+        $(".nav-items").append("<div class='nav-item nav-appended-dropdown'>" + $(this).html() + "</div>");
+    });
+}
+
+function removeNavDropdownLinks() {
+    console.log("remove");
+    $(".nav-appended-dropdown").each(function() {
+        $(this).remove();
+    });
+}
---
+++ test-site/script.js
@@ -0,0 +1,11 @@
+$("#typography-link").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#typography").offset().top
+    }, 500);
+});
+
+$("#grid-link").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#grid").offset().top
+    }, 500);
+});
