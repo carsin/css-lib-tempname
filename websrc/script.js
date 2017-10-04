b00a20af18d8e78cfa1deb0fdaca1ccbde45d4b8
carson <carsin@users.noreply.github.com>
2017-10-04 12:05:48 -1000

    demo: adaptive nav logo fix

--- websrc/script.js
+++ websrc/script.js
@@ -1,6 +1,6 @@
 $(window).scroll(function() {
     // logos.png
-    if ($(".nav-items .nav-item a").css("color") === "black") {
+    if ($(".nav-items .nav-item a").css("color") === "rgb(0, 0, 0)") {
         $("#nav-adaptive-icon").attr("src", "websrc/img/logos.png");
     } else {
         $("#nav-adaptive-icon").attr("src", "websrc/img/logo-white.png");
