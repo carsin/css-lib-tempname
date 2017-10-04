974298dfbacac48224f1efd76c749cce3bf732da
carsin <carsin@users.noreply.github.com>
2017-10-03 16:03:04 -1000

    general: update from full.css & full.js

--- dist/modules/nav/nav.js
+++ dist/modules/nav/nav.js
@@ -17,13 +17,13 @@ $(document).ready(function() {
             clicked = true;
         }
     });
+    scrollEvent();
 });
 
 // Remove navbar when clicking on a link
 function hideNav() {
-	if ($(window).width() < 799){
+	if ($(window).width() <= 800){
   	    toggleNavElements();
-        console.log("click");
         removeNavDropdownLinks();
     }
 }
@@ -53,44 +53,78 @@ function removeNavDropdownLinks() {
 
 // Scrollspy variables
 var lastId;
-var nav = $(".nav-scrollspy");
+var nav = $(".nav");
 var navHeight = nav.outerHeight();
-var navItems = $(".nav-items").find(".nav-item a");
+var navItems = $(".nav").find(".nav-item a");
 var scrollItems = navItems.map(function() {
     var item = $($(this).attr("href"));
     if (item.length) { return item; }
 });
 
-
-// Default smooth scroll
-navItems.click(function(e) {
-    var href = $(this).attr("href");
-    $('html, body').stop().animate({ 
-        scrollTop: $(href).offset().top - navHeight
-    }, 400);
-    e.preventDefault();
-});
-
 // Scroll event
-$(window).scroll(function(){
-    // Get container scroll position
-    var fromTop = $(this).scrollTop() + navHeight;
+$(window).scroll(scrollEvent);
 
-    // Get id of current scroll item
-    
-    var cur = scrollItems.map(function(){
-        if ($(this).offset().top < fromTop)
-        return this;
-    });
+function scrollEvent() {
+    if (nav.hasClass("nav-scrollspy") || nav.hasClass("nav-background-adapt")) {
+        // Get container scroll position
+        var fromTop = $(this).scrollTop() + navHeight + 1;
 
-    // Get the id of the current element
-    
-    cur = cur[cur.length-1];
-    var id = cur && cur.length ? cur[0].id : "";
+        // Get id of current scroll item
+        var cur = scrollItems.map(function() {
+            if ($(this).offset().top < fromTop)
+            return this;
+        });
 
-    if (lastId !== id) {
-        lastId = id;
-        // Set / remove active class
-        navItems.parent().removeClass("active").end().filter("[href='#"+id+"']").parent().addClass("active");
-   }
-});
+        // Get the id of the current element
+        cur = cur[cur.length-1];
+        var id = cur && cur.length ? cur[0].id : "";
+
+        if (nav.hasClass("nav-scrollspy")) {
+            if (lastId !== id) {
+                lastId = id;
+                // Set / remove active class
+                navItems.parent().removeClass("active").end().filter("[href='#"+id+"']").parent().addClass("active");
+            }
+        }
+
+        if (nav.hasClass("nav-background-adapt")) {
+            if (nav.css("background-color") !== $("#" + id).css("background-color")) {
+                var itemColor = $("#" + id).css("background-color");
+
+                // If item is transparent, fuck that shit. Use body as fallback color.
+                if (itemColor.includes("rgba")) { itemColor = $("body").css("background-color"); }
+
+                // Set background color of nav & nav items to current section's background
+                nav.css("background-color", itemColor);
+                $(".nav-items").css("background-color", itemColor);
+                $(".nav-items").find(".nav-dropdown-item ul").css("background-color", itemColor);
+
+                var navColor = nav.css("background-color");
+                var navColorsOnly = navColor.substring(navColor.indexOf('(') + 1, navColor.lastIndexOf(')')).split(/,\s*/);
+
+                navItems.css("color", determineTextColor(navColorsOnly[0], navColorsOnly[1], navColorsOnly[2]));
+                $(".nav").find(".nav-dropdown-title").css("color", determineTextColor(navColorsOnly[0], navColorsOnly[1], navColorsOnly[2]));
+                $(".nav").find(".nav-dropdown-item ul li a").css("color", determineTextColor(navColorsOnly[0], navColorsOnly[1], navColorsOnly[2]));
+                $(".nav").find(".toggle-line").css("background-color", determineTextColor(navColorsOnly[0], navColorsOnly[1], navColorsOnly[2]));
+            }
+        }
+    }
+}
+
+// Determines whether color should be black or white based on input rgb 
+function determineTextColor(red, green, blue) {
+    var c = [red/255, green/255, blue/255];
+
+    for (var i = 0; i < c.length; i++) {
+        // Disables jshint for this line
+        // jshint ignore:start 
+        c[i] <= 0.03928 ?  c[i] = c[i] / 12.92 : c[i] = Math.pow((c[i] + 0.055) / 1.055, 2.4);
+        // jshint ignore:end 
+    }
+
+    if (0.2126 * c[0] + 0.7152 * c[1] + 0.0722 * c[2] > 0.179) {
+        return "black";
+    } else {
+        return "white";
+    }
+}
